97b7cad7cb4e4d38c087213cf53ecd17262df946
harambe4president <al.hartmann@student.uni-tuebingen.de>
2017-02-18 12:36:10 +0100

    euclidian distance included.geil

--- src/main/scala/com/ir/KMCluster.scala
+++ src/main/scala/com/ir/KMCluster.scala
@@ -50,7 +50,15 @@ class KMCluster(num_of_clusters: String) {
 
 
   def euclidDistance(vector1: Vector[Float], vector2: Vector[Float]): Float = {
-    Float.MinValue //TODO Implementation of calculation
+
+    var distance: Float = 0
+    for(index <- 0 until vector1.length){
+      distance += square(vector1(index) - vector2(index))
+    }
+
+    Math.sqrt(distance).toFloat
+
+    def square(x: Float) = x * x
   }
 
 
